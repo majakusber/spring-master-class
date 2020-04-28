08fabe4fa1e299f8c8295ba78fc1263e4d5a5de4
Maria Kusber <maria.kusber@diffblue.com>
2020-04-28 15:39:23 +0100

    Use @Repository and @Service for data and business layer components

--- demo/src/main/java/com/spring/tutorial/basics/demo/scope/PersonDAO.java
+++ demo/src/main/java/com/spring/tutorial/basics/demo/scope/PersonDAO.java
@@ -1,9 +1,9 @@
 package com.spring.tutorial.basics.demo.scope;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class PersonDAO {
 
   @Autowired
